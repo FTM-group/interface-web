70bb9b238122c5cd78cae63a3c1b1d2fd2f26674
FTM-group <nonok2708@gmail.com>
2018-12-27 11:02:41 +0100

    Installation de twig

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once 'vendor/autoload.php';
+
+$loader = new Twig_Loader_Filesystem('templates');
+$twig = new Twig_Environment($loader, array(
+    // 'cache' => 'compilation_cache',
+));
+
+echo $twig->render('index.html', array('name' => 'nonok'));
+
+
