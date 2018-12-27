8c56eeb16137073efa36d3dd1707ddac26102dd0
FTM-group <nonok2708@gmail.com>
2018-12-27 11:09:51 +0100

    Ajout d'un .gitignore

--- index.php
+++ index.php
@@ -4,9 +4,10 @@ require_once 'vendor/autoload.php';
 
 $loader = new Twig_Loader_Filesystem('templates');
 $twig = new Twig_Environment($loader, array(
-    // 'cache' => 'compilation_cache',
+    'cache' => 'compilation_cache',
+    // 'cache' => false,
 ));
 
-echo $twig->render('index.html', array('name' => 'nonok'));
+echo $twig->render('index.html', array('name' => 'Sooo cool !!:'));
 
 
